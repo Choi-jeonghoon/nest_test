f34df9d45fe02778887f96d8775b6c806aff7f05
최정훈 <jeongssi94@gmail.com>
2022-10-07 21:54:32 +0900

    Add: 삭제 업데이트 test완료

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -11,6 +11,11 @@ describe('MoviesService', () => {
     }).compile();
 
     service = module.get<MoviesService>(MoviesService);
+    service.create({
+      title: 'Test Movie',
+      genres: ['test'],
+      year: 2000,
+    });
   });
 
   it('should be defined', () => {
@@ -45,6 +50,64 @@ describe('MoviesService', () => {
       } catch (e) {
         expect(e).toBeInstanceOf(NotFoundException);
         expect(e.message).toEqual('Movie with ID 99 not found');
+      }
+    });
+  });
+
+  describe('deletOne', () => {
+    it('delets a movie', () => {
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 2000,
+      });
+      const beforeDelete = service.getAll().length;
+      service.deleteOnd(1);
+      const afterDelete = service.getAll().length;
+
+      expect(afterDelete).toBeLessThan(beforeDelete);
+      // expect(afterDelete).toEqual(allMovies - 1); 위 코드와 같은 의미를 가진다.
+      // aftertDelte.length가 allMovies.length - 1 와 같아야 된다는 것(한 개가 삭제됐으므로 movies배열의 길이가 -1이 되어야한다는 뜻
+    });
+    it('should retun a 404', () => {
+      try {
+        service.deleteOnd(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
+
+  describe('create', () => {
+    it('should  create a movice', () => {
+      const beforeCreate = service.getAll().length;
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 2000,
+      });
+      const afterCreate = service.getAll().length;
+      console.log(beforeCreate, afterCreate);
+      expect(afterCreate).toBeGreaterThan(beforeCreate);
+    });
+  });
+
+  describe('update', () => {
+    it('should update a movice', () => {
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 2000,
+      });
+      service.update(1, { title: 'Updated Test' });
+      const movice = service.getOne(1);
+      expect(movice.title).toEqual('Updated Test');
+    });
+    it('should throw a NotFoundException', () => {
+      try {
+        service.update(999, {});
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
       }
     });
   });
